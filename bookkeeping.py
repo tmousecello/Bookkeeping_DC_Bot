dcfd61d9bc04f9a5123ba6c22abf51256ddef2b1
tmouse <jonathanhsu@gapp.nthu.edu.tw>
2023-09-09 17:08:12 +0800

    Add the main code

--- bookkeeping.py
+++ bookkeeping.py
@@ -67,7 +67,25 @@ cost = 0
 income = 0
 
 @bot.command()
-async def expenses(ctx: commands.Context, name: str, price: int):
+async def clear(ctx: commands.Context):
+    with open('board.csv', mode = 'w', newline='') as csvfile: # mode = a 是一直加上去 mode=w 是覆寫
+        # 建立 CSV 檔寫入器
+        writer = csv.writer(csvfile)
+        writer.writerow(["InOut","month","date","category1","name","price"])
+    with open('expense.csv', mode = 'w', newline='') as csvfile: # mode = a 是一直加上去 mode=w 是覆寫
+        # 建立 CSV 檔寫入器
+        writer = csv.writer(csvfile)
+        writer.writerow(["InOut","month","date","category1","name","price"])
+    with open('income.csv', mode = 'w', newline='') as csvfile: # mode = a 是一直加上去 mode=w 是覆寫
+        # 建立 CSV 檔寫入器
+        writer = csv.writer(csvfile)
+        writer.writerow(["InOut","month","date","category1","name","price"])
+        await ctx.send("clear")
+    
+
+
+@bot.command()
+async def expense(ctx: commands.Context, name: str, price: int):
     global money
     global cost
     global income
@@ -77,23 +95,59 @@ async def expenses(ctx: commands.Context, name: str, price: int):
     month, date = currentDateAndTime.month,currentDateAndTime.day
     category1 = "everything"
 
+    # 寫入總表
     with open('board.csv', mode = 'a', newline='') as csvfile: # mode = a 是一直加上去 mode=w 是覆寫
         # 建立 CSV 檔寫入器
         writer = csv.writer(csvfile)
-        # 寫入支出
-        writer.writerow([InOut, month, date, category1, name, price])
+        # 寫入支出到總表
+        writer.writerow([InOut, month, date, category1, name, "-"+str(price)])
     
+    # 寫入支出
+    with open('expense.csv', mode = 'a', newline='') as csvfile: # mode = a 是一直加上去 mode=w 是覆寫
+        # 建立 CSV 檔寫入器
+        writer = csv.writer(csvfile)
+        # 寫入支出到支出表
+        writer.writerow([InOut, month, date, category1, name, int(price)])
+
     # 計算csv
-    costLs = pd.read_csv("board.csv")
+    costLs = pd.read_csv("expense.csv")
     cost = sum(costLs["price"]) # 計算總花費
-
-
-    await ctx.send(f"You buy {name}, Your money remain {money-cost+income}")
-    
+    await ctx.send(f"You buy {name}, it cost {price}\nYour money remain {money-int(cost)+int(income)}")
+    print(f'Buy {name}, spent{price}')
     print()
 
 
+@bot.command()
+async def revenue(ctx: commands.Context, name: str, price: int):
+    global money
+    global cost
+    global income
 
+    InOut = "In"
+    currentDateAndTime = datetime.now()
+    month, date = currentDateAndTime.month,currentDateAndTime.day
+    category1 = "everything"
+
+    # 寫入總表
+    with open('board.csv', mode = 'a', newline='') as csvfile: # mode = a 是一直加上去 mode=w 是覆寫
+        # 建立 CSV 檔寫入器
+        writer = csv.writer(csvfile)
+        # 寫入支出到總表
+        writer.writerow([InOut, month, date, category1, name, "+"+str(price)])
+    
+    # 寫入收入
+    with open('income.csv', mode = 'a', newline='') as csvfile: # mode = a 是一直加上去 mode=w 是覆寫
+        # 建立 CSV 檔寫入器
+        writer = csv.writer(csvfile)
+        # 寫入支出到支出表
+        writer.writerow([InOut, month, date, category1, name, int(price)])
+    # 計算csv
+    incomeLs = pd.read_csv("income.csv")
+    income = sum(incomeLs["price"]) # 計算總花費
+    await ctx.send(f"You get {price} for your {name}.\nYour money remain {money-cost+income}")
+
+    print(f'Got {price}')
+    print()
 
 # Run the Discord BOT
 if __name__ == "__main__":
